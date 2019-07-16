95460d6635706ec86aa95ab4d67ff93f7ae39a25
ndemeyvan <ndemeyvan@gmail.com>
2019-07-16 21:13:45 +0200

    error fix

--- CommunityMarket/app/src/main/java/cm/studio/devbee/communitymarket/CheckNetwork.java
+++ CommunityMarket/app/src/main/java/cm/studio/devbee/communitymarket/CheckNetwork.java
@@ -7,31 +7,21 @@ import android.util.Log;
 
 public class CheckNetwork {
 
-
-    private static final String TAG = CheckNetwork.class.getSimpleName();
-
-
-
-    public static boolean isInternetAvailable(Context context)
-    {
+    public static boolean isInternetAvailable(Context context) {
         NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
-
         if (info == null)
         {
-            Log.d(TAG,"no internet connection");
             return false;
         }
         else
         {
             if(info.isConnected())
             {
-                Log.d(TAG," internet connection available...");
                 return true;
             }
             else
             {
-                Log.d(TAG," internet connection");
                 return true;
             }
 
